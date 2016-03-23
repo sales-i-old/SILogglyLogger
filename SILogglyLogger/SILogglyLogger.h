5a2c4f6278d623240ec2384e8acb3f01143d128a
Dillon Hoa <mr_d_2chat@hotmail.com>
2016-03-23 10:13:45 +0000

    Initial Commit

---
+++ SILogglyLogger/SILogglyLogger.h
@@ -0,0 +1,13 @@
+//
+//  SILogglyLogger.h
+//  SILogglyLogger
+//
+//  Created by Dillon Hoa on 23/03/2016.
+//  Copyright © 2016 sales-i. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SILogglyLogger : NSObject
+
+@end
---
+++ SILogglyLogger/SILogglyLogger.m
@@ -0,0 +1,13 @@
+//
+//  SILogglyLogger.m
+//  SILogglyLogger
+//
+//  Created by Dillon Hoa on 23/03/2016.
+//  Copyright © 2016 sales-i. All rights reserved.
+//
+
+#import "SILogglyLogger.h"
+
+@implementation SILogglyLogger
+
+@end
